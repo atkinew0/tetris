9d3a13951269274edc5839273c9659c74f06cf95
Eli Atkinson <atkinson.eli@gmail.com>
2019-10-15 19:56:19 -0700

    Updates

---
+++ precache-manifest.56f59345c34413d5441ffa7d720acae4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04a33e7c0a6a850b2e1d23ad2a32c1ca",
+    "url": "/tetris/index.html"
+  },
+  {
+    "revision": "d251bb8be58d4f5980ba",
+    "url": "/tetris/static/css/main.ad922669.chunk.css"
+  },
+  {
+    "revision": "d3c77bd75fc565ec511f",
+    "url": "/tetris/static/js/2.99e9e5f5.chunk.js"
+  },
+  {
+    "revision": "d251bb8be58d4f5980ba",
+    "url": "/tetris/static/js/main.6eec67ef.chunk.js"
+  },
+  {
+    "revision": "d8d323ca928f7e4354e1",
+    "url": "/tetris/static/js/runtime-main.b1d22937.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/tetris/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "2810fe6b5435e75b3dad63e341b97d02",
+    "url": "/tetris/static/media/bg.2810fe6b.png"
+  }
+]);
